f99d48ea8a9dd979f29830037cbbbeb4fd85ac79
Andrewjjj <Juwon1@ualberta.ca>
2019-11-01 15:24:10 -0600

    Success Test

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -3,19 +3,19 @@ package com.example.simpleparadox.listycity;
 public class City implements Comparable<City>{
     private String city;
     private String province;
-//
-//    City(String city, String province){
-//        this.city = city;
-//        this.province = province;
-//    }
-//
-//    String getCityName(){
-//        return this.city;
-//    }
-//
-//    String getProvinceName(){
-//        return this.province;
-//    }
+
+    City(String city, String province){
+        this.city = city;
+        this.province = province;
+    }
+
+    String getCityName(){
+        return this.city;
+    }
+
+    String getProvinceName(){
+        return this.province;
+    }
 
     @Override
     public int compareTo(City o) {
